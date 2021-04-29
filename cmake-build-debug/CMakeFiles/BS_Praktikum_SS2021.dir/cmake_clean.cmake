5310d18cf34549067424df47ecdb7920747b07d1
Joost Groot Bramel <joost-henning.groot_bramel@smail.th-koeln.de>
2021-04-29 18:42:46 +0200

    Innitial Commit

---
+++ cmake-build-debug/CMakeFiles/BS_Praktikum_SS2021.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BS_Praktikum_SS2021.exe"
+  "BS_Praktikum_SS2021.exe.manifest"
+  "BS_Praktikum_SS2021.pdb"
+  "CMakeFiles/BS_Praktikum_SS2021.dir/main.c.obj"
+  "libBS_Praktikum_SS2021.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BS_Praktikum_SS2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
